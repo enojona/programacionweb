ea154790511e3058d5079d1e6fec5ffd478409bd
enojona <karea.221997@gmail.com>
2016-08-29 19:45:02 -0500

    programacionweb

---
+++ 29-08-2016--2.php
@@ -0,0 +1,4 @@
+
+<h1>inclusion de archivos</h1>
+<p> contenido principal </p>
+
---
+++ 29-08-2016--3.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+<?php require ("29-08-2016--2.php");?>
+<p> contenido secundario a relivo </p>
+
+</body>
+
+</html>
